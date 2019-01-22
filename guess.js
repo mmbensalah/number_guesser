bb2d015efc32017db3cfe6543a54d5ff99ac62d6
mmbensalah <mmbensalah@gmail.com>
2019-01-22 13:43:29 -0700

    f

--- guess.js
+++ guess.js
@@ -1,49 +1,60 @@
-let randomNumber = Math.floor(Math.random() * 10)
+var min = document.querySelector(".minRange")
+var max = document.querySelector(".maxRange")
+var userGuess = document.querySelector(".guessField")
 
-$( document ).ready(function() {
-  $("#guessSubmit").click(function() {
-    let userGuess = parseInt($(".guessField").val());
-    const checkGuess = (userGuess) => {
-      if (isNaN(userGuess)) {
-        alert("Not a number, try again");
-      } else if(11 < userGuess || userGuess < 0) {
-        alert("Number out of range, try again");
-      } else if(userGuess > randomNumber) {
-        event.preventDefault();
-        $(".lastGuess").text(userGuess);
-        $(".highLow").text("Too high");
-      } else if (userGuess < randomNumber) {
-        event.preventDefault();
-        $(".lastGuess").text(userGuess);
-        $(".highLow").text("Too low");
-      } else {
-        event.preventDefault();
-        $(".message").text("Boom!");
-        $(".lastGuess").text("");
-        $(".highLow").text("");
-      }
-    };
-    checkGuess(userGuess)
-  });
-  $("#guessClear").click(function() {
-    $(".guessField").text("");
-  });
-  $("#reset").click(function() {
-    $(".guessField").text("");
-    randomNumber = Math.floor(Math.random() * 10)
-  });
-  $(function () {
-    $('.guessField').keyup(function () {
-      if ($(this).val() == '') {
-          $('#guessClear').prop('disabled', true);
-          $('#reset').prop('disabled', false);
-      } else {
-          $('#guessClear').prop('disabled', false);
-          $('#reset').prop('disabled', true);
-      }
-    });
-  });
-});
+function run(){
+  ("#guessSubmit").addEventListener("click", checkGuess())
+}
+
+
+
+
+// let randomNumber = Math.floor(Math.random() * 10)
+//
+// $( document ).ready(function() {
+//   $("#guessSubmit").click(function() {
+//     let userGuess = parseInt($(".guessField").val());
+//     const checkGuess = (userGuess) => {
+//       if (isNaN(userGuess)) {
+//         alert("Not a number, try again");
+//       } else if(11 < userGuess || userGuess < 0) {
+//         alert("Number out of range, try again");
+//       } else if(userGuess > randomNumber) {
+//         event.preventDefault();
+//         $(".lastGuess").text(userGuess);
+//         $(".highLow").text("Too high");
+//       } else if (userGuess < randomNumber) {
+//         event.preventDefault();
+//         $(".lastGuess").text(userGuess);
+//         $(".highLow").text("Too low");
+//       } else {
+//         event.preventDefault();
+//         $(".message").text("Boom!");
+//         $(".lastGuess").text("");
+//         $(".highLow").text("");
+//       }
+//     };
+//     checkGuess(userGuess)
+//   });
+//   $("#guessClear").click(function() {
+//     $(".guessField").text("");
+//   });
+//   $("#reset").click(function() {
+//     $(".guessField").text("");
+//     randomNumber = Math.floor(Math.random() * 10)
+//   });
+//   $(function () {
+//     $('.guessField').keyup(function () {
+//       if ($(this).val() == '') {
+//           $('#guessClear').prop('disabled', true);
+//           $('#reset').prop('disabled', false);
+//       } else {
+//           $('#guessClear').prop('disabled', false);
+//           $('#reset').prop('disabled', true);
+//       }
+//     });
+//   });
+// });
 
 // $("#minMaxBtn").click(function() {
 //   let min = parseInt($(".minRange").val());
